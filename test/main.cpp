54652d71d70ddf727947c5d07e4915cdb81016df
nakat-t <armaiti.wizard@gmail.com>
2019-02-20 16:32:55 +0900

    Fix warnings of test code

--- test/main.cpp
+++ test/main.cpp
@@ -5,9 +5,6 @@
 // Define `ENDIAN_IS_LITTLE` or `ENDIAN_IS_BIG` macro before include "endian.h".
 // Alternatively, if you define the `ENDIAN_DETECT` macro, it detects endianness
 // at run time. In that case, the speed will be slow.
-#define ENDIAN_IS_LITTLE
-//#define ENDIAN_IS_BIG
-//#define ENDIAN_DETECT
 #include <endian/endian.h>
 
 #include <cassert>
@@ -45,7 +42,7 @@ int main()
     }
     { // hton16(),ntoh16() should convert between host byte order and network byte order.
         uc<uint16_t> x;
-        for(int i=0; i<sizeof(x); ++i) x.c[i] = i+1;
+        for(size_t i=0; i<sizeof(x); ++i) x.c[i] = i+1;
         assert(endian::hton16(uint16_t{0x0102}) == x.i);
         assert(endian::hton<uint16_t>(uint16_t{0x0102}) == x.i);
         assert(endian::ntoh16(x.i) == uint16_t{0x0102});
@@ -53,7 +50,7 @@ int main()
     }
     { // hton32(),ntoh32() should convert between host byte order and network byte order.
         uc<uint32_t> x;
-        for(int i=0; i<sizeof(x); ++i) x.c[i] = i+1;
+        for(size_t i=0; i<sizeof(x); ++i) x.c[i] = i+1;
         assert(endian::hton32(uint32_t{0x01020304}) == x.i);
         assert(endian::hton<uint32_t>(uint32_t{0x01020304}) == x.i);
         assert(endian::ntoh32(x.i) == uint32_t{0x01020304});
@@ -61,7 +58,7 @@ int main()
     }
     { // hton64(),ntoh64() should convert between host byte order and network byte order.
         uc<uint64_t> x;
-        for(int i=0; i<sizeof(x); ++i) x.c[i] = i+1;
+        for(size_t i=0; i<sizeof(x); ++i) x.c[i] = i+1;
         assert(endian::hton64(uint64_t{0x0102030405060708}) == x.i);
         assert(endian::hton<uint64_t>(uint64_t{0x0102030405060708}) == x.i);
         assert(endian::ntoh64(x.i) == uint64_t{0x0102030405060708});
@@ -73,7 +70,7 @@ int main()
     }
     { // big::to_host16(),from_host16() converts between big endian and host endian.
         uc<uint16_t> x;
-        for(int i=0; i<sizeof(x); ++i) x.c[i] = i+1;
+        for(size_t i=0; i<sizeof(x); ++i) x.c[i] = i+1;
         assert(endian::big::from_host16(uint16_t{0x0102}) == x.i);
         assert(endian::big::from_host<uint16_t>(uint16_t{0x0102}) == x.i);
         assert(endian::big::to_host16(x.i) == uint16_t{0x0102});
@@ -81,7 +78,7 @@ int main()
     }
     { // big::to_host32(),from_host32() converts between big endian and host endian.
         uc<uint32_t> x;
-        for(int i=0; i<sizeof(x); ++i) x.c[i] = i+1;
+        for(size_t i=0; i<sizeof(x); ++i) x.c[i] = i+1;
         assert(endian::big::from_host32(uint32_t{0x01020304}) == x.i);
         assert(endian::big::from_host<uint32_t>(uint32_t{0x01020304}) == x.i);
         assert(endian::big::to_host32(x.i) == uint32_t{0x01020304});
@@ -89,7 +86,7 @@ int main()
     }
     { // big::to_host64(),from_host64() converts between big endian and host endian.
         uc<uint64_t> x;
-        for(int i=0; i<sizeof(x); ++i) x.c[i] = i+1;
+        for(size_t i=0; i<sizeof(x); ++i) x.c[i] = i+1;
         assert(endian::big::from_host64(uint64_t{0x0102030405060708}) == x.i);
         assert(endian::big::from_host<uint64_t>(uint64_t{0x0102030405060708}) == x.i);
         assert(endian::big::to_host64(x.i) == uint64_t{0x0102030405060708});
@@ -101,7 +98,7 @@ int main()
     }
     { // little::to_host16(),from_host16() converts between little endian and host endian.
         uc<uint16_t> x;
-        for(int i=0; i<sizeof(x); ++i) x.c[i] = sizeof(x)-i;
+        for(size_t i=0; i<sizeof(x); ++i) x.c[i] = sizeof(x)-i;
         assert(endian::little::from_host16(uint16_t{0x0102}) == x.i);
         assert(endian::little::from_host<uint16_t>(uint16_t{0x0102}) == x.i);
         assert(endian::little::to_host16(x.i) == uint16_t{0x0102});
@@ -109,7 +106,7 @@ int main()
     }
     { // little::to_host32(),from_host32() converts between big endian and host endian.
         uc<uint32_t> x;
-        for(int i=0; i<sizeof(x); ++i) x.c[i] = sizeof(x)-i;
+        for(size_t i=0; i<sizeof(x); ++i) x.c[i] = sizeof(x)-i;
         assert(endian::little::from_host32(uint32_t{0x01020304}) == x.i);
         assert(endian::little::from_host<uint32_t>(uint32_t{0x01020304}) == x.i);
         assert(endian::little::to_host32(x.i) == uint32_t{0x01020304});
@@ -117,7 +114,7 @@ int main()
     }
     { // little::to_host64(),from_host64() converts between big endian and host endian.
         uc<uint64_t> x;
-        for(int i=0; i<sizeof(x); ++i) x.c[i] = sizeof(x)-i;
+        for(size_t i=0; i<sizeof(x); ++i) x.c[i] = sizeof(x)-i;
         assert(endian::little::from_host64(uint64_t{0x0102030405060708}) == x.i);
         assert(endian::little::from_host<uint64_t>(uint64_t{0x0102030405060708}) == x.i);
         assert(endian::little::to_host64(x.i) == uint64_t{0x0102030405060708});
